83d8fe4094c326f508d267f6f4e187a29a423ee8
Matt Bruer <mbruer@gmail.com>
2023-06-12 13:26:10 -0500

    add drink img

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,45 @@
-export default function Home() {
+import axios from "axios";
+import ExpenseTracker from "@/components/ExpenseTracker";
+import Image from "next/image";
+
+// async function getTest() {
+//   const res = await axios.get("http://localhost:8000/api/test");
+//   return res.data;
+// }
+
+// async function postTest() {
+//   try {
+//     const res = await axios.post("http://localhost:8000/api/test", {
+//       name: `mattbruer${Math.floor(Math.random() * 1000)}`,
+//     });
+//     return res.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+export default async function Home() {
+  // const [test] = await Promise.all([getTest()]);
+
+  // await postTest();
   return (
     <div className="m-5">
+      <h1 id="hello">hello</h1>
+      {/* <p>{JSON.stringify(test)}</p> */}
+      <Image
+        priority
+        src="/trezo-12.jpg"
+        alt="headshot"
+        width="0"
+        height="0"
+        sizes="300px"
+        style={{ width: "300px", height: "auto" }}
+      />
       <h1 className="text-8xl underline mb-5">Life</h1>
-      <p>Expense Tracker</p>
-      <p>bills paid and unpaid</p>
+
+      <ExpenseTracker />
+
+      {/* <p>bills paid and unpaid</p>
       <p>health</p>
       <p>food</p>
       <p>gf</p>
@@ -14,7 +50,7 @@ export default function Home() {
       <p>calendar</p>
       <p>job</p>
       <p>music</p>
-      <p>stickies</p>
+      <p>stickies</p> */}
     </div>
   );
 }
---
+++ components/ExpenseTracker.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExpenseTracker = () => {
+  return <div>Expense Tracker</div>;
+};
+
+export default ExpenseTracker;
